074d51939249d049ada29f38e289e6960a20b9f9
mjarkk <mkopenga@gmail.com>
2019-04-21 13:59:37 +0200

    Made a working search

---
+++ js/master.js
@@ -0,0 +1,16 @@
+let search = document.querySelector(".ddg_search")
+let searchButton = document.querySelector(".ddg_search_button")
+
+searchButton.onclick = function(event) {
+  doASearch()
+}
+
+search.onkeydown = function(event) {
+  if (event.key == 'Enter') {
+    doASearch()
+  }
+}
+
+function doASearch() {
+  location.href = 'https://duckduckgo.com/?q=' + search.value.replace(/\s/g, '+')
+}
