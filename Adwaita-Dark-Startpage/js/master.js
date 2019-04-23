72751b103465258d34700efc608722bc4c166209
Dennis ten Hoove <dennistenhoove@protonmail.com>
2019-04-23 04:39:58 +0200

    click anywhere to select search and auto select search on page open

--- Adwaita-Dark-Startpage/js/master.js
+++ Adwaita-Dark-Startpage/js/master.js
@@ -1,5 +1,8 @@
-let search = document.querySelector(".ddg_search")
-let searchButton = document.querySelector(".ddg_search_button")
+let search = document.querySelector('.ddg_search')
+let searchButton = document.querySelector('.ddg_search_button')
+
+search.focus()
+search.select()
 
 searchButton.onclick = function(event) {
   doASearch()
@@ -13,4 +16,9 @@ search.onkeydown = function(event) {
 
 function doASearch() {
   location.href = 'https://duckduckgo.com/?q=' + search.value.replace(/\s/g, '+')
+}
+
+function selectSearch() {
+  search.focus()
+  search.select()
 }
